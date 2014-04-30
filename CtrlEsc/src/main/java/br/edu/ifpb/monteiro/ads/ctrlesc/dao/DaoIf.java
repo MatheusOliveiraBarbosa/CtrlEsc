21d7d55cb021a004d641663833a04f71e99b184a
E.Wellington <E.Wellington@Note-Well>
2014-04-29 21:57:10 -0300

    Configuration of glassfish

---
+++ CtrlEsc/src/main/java/br/edu/ifpb/monteiro/ads/ctrlesc/dao/DaoIf.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifpb.monteiro.ads.ctrlesc.dao;
+
+import br.edu.ifpb.monteiro.ads.ctrlesc.util.JPAUtil;
+
+/**
+ *
+ * @author E.Wellington
+ */
+public class DaoIf {
+    
+    
+    public static void main(String[] args) {
+        JPAUtil.getEntityManager();
+        
+    }
+}
