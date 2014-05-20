f9cd9d28bd9ea12edb9ced9c0a3ae5c3495e5727
E.Wellington <E.Wellington@Note-Well>
2014-05-20 01:04:33 -0300

    Add file useCase diagram

--- CtrlEsc/src/main/java/br/edu/ifpb/monteiro/ads/ctrlesc/dao/DaoImpl.java
+++ CtrlEsc/src/main/java/br/edu/ifpb/monteiro/ads/ctrlesc/dao/DaoImpl.java
@@ -1,61 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.edu.ifpb.monteiro.ads.ctrlesc.dao;
 
 import br.edu.ifpb.monteiro.ads.ctrlesc.model.Identifiable;
-import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 /**
  *
  * @author Matheus
+ * @param <T>
  */
-public class DaoImpl {
-    
+public class DaoImpl<T> implements GenericDAO<Identifiable> {
+
     @PersistenceContext(unitName = "CtrlEsc")
     private EntityManager entityManager;
-    
-        
-    
-    public void create(Identifiable entity) {
-       
-        try{
-           entityManager.getTransaction().begin();
-           entityManager.persist(entity);
-           entityManager.getTransaction().commit();
-        }catch(EntityExistsException e){
-            
+
+    @Override
+    public void save(Identifiable entity) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(entity);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
             entityManager.getTransaction().rollback();
         }
-        
     }
 
-    public Identifiable read() {
-       
-        return null;
-        
+    @Override
+    public Identifiable<Identifiable> getById(Class<Identifiable> classe, Long id) {
+        return entityManager.find(classe, id);
     }
 
-    public void update(Identifiable entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @Override
+    public Identifiable<Identifiable> update(Identifiable entity) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.refresh(entity);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+        }
+
+        return entity;
     }
 
+    @Override
     public void delete(Identifiable entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.remove(entity);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+        }
     }
 
-    public EntityManager getEntityManager() {
-        return entityManager;
-    }
-
-    public void setEntityManager(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-    
-        
 }
--- CtrlEsc/src/main/java/br/edu/ifpb/monteiro/ads/ctrlesc/dao/GenericDAO.java
+++ CtrlEsc/src/main/java/br/edu/ifpb/monteiro/ads/ctrlesc/dao/GenericDAO.java
@@ -1,26 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.edu.ifpb.monteiro.ads.ctrlesc.dao;
 
 import br.edu.ifpb.monteiro.ads.ctrlesc.model.Identifiable;
 
 /**
- *
  * @author Matheus
+ * @param <T>
  */
 public interface GenericDAO<T extends Identifiable> {
     
-    void create (Identifiable entity);
+    void save (Identifiable entity);
     
-    Identifiable<T> read();
+    Identifiable<T> getById(Class<T> classe ,Long id);
+        
+    Identifiable<T> update(Identifiable entity);
     
-    void update(Identifiable entity);
-    
-    void delete (Identifiable entity);
+    void delete(Identifiable entity);
     
     
     
